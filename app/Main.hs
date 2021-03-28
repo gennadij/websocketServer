049d689b04495f6972e5c3e31021ad1e9571f5f3
Gennadi <gennadiheimann@gmail.com>
2021-03-29 00:42:08 +0200

    weitere Tests

--- app/Main.hs
+++ app/Main.hs
@@ -26,66 +26,23 @@ import qualified Data.Text.IO as T
 
 import qualified Network.WebSockets as WS
 
--- We represent a client by their username and a `WS.Connection`. We will see how we
--- obtain this `WS.Connection` later on.
-
-type Client = (Text, WS.Connection)
-
--- The state kept on the server is simply a list of connected clients. We've added
--- an alias and some utility functions, so it will be easier to extend this state
--- later on.
-
-type ServerState = [Client]
-
 -- new TestTyp
 type Clients = [WS.Connection]
--- Create a new, initial state:
-
-newServerState :: ServerState
-newServerState = []
 
 -- newClient
 newClients :: Clients
 newClients = []
 -- Get the number of active clients:
 
-numClients :: ServerState -> Int
-numClients = length
-
--- Check if a user already exists (based on username):
-
-clientExists :: Client -> ServerState -> Bool
-clientExists client = any ((== fst client) . fst)
-
--- Add a client (this does not check if the client already exists, you should do
--- this yourself using `clientExists`):
-
-addClient :: Client -> ServerState -> ServerState
-addClient client clients = client : clients
 
 -- new addClient
 addClient_ :: WS.Connection -> Clients -> Clients
 addClient_ conn clients = conn : clients
 
--- Remove a client:
-
-removeClient :: Client -> ServerState -> ServerState
-removeClient client = filter ((/= fst client) . fst)
-
--- Send a message to all clients, and log it on stdout:
-
-broadcast :: Text -> ServerState -> IO ()
-broadcast message clients = do
-    T.putStrLn message
-    forM_ clients $ \(_, conn) -> WS.sendTextData conn message
-
--- The main function first creates a new state for the server, then spawns the
--- actual server. For this purpose, we use the simple server provided by
--- `WS.runServer`.
-
 main :: IO ()
 main = do
 --    state <- newMVar newServerState
+    T.putStrLn "Server started."
     clients <- newMVar newClients
     WS.runServer "127.0.0.1" 9160 $ application clients
 
@@ -93,58 +50,19 @@ main = do
 
 application :: MVar Clients -> WS.ServerApp
 
--- Note that `WS.ServerApp` is nothing but a type synonym for
--- `WS.PendingConnection -> IO ()`.
-
--- Our application starts by accepting the connection. In a more realistic
--- application, you probably want to check the path and headers provided by the
--- pending request.
-
--- We also fork a pinging thread in the background. This will ensure the connection
--- stays alive on some browsers.
-
--- application state pending = do
 application clients pending = do
   conn <- WS.acceptRequest pending
   WS.withPingThread conn 30 (return ()) $ do
-    msg <- WS.receiveData conn
---    T.putStrLn (T.append (T.pack("Der Client sendet: ")) msg)
-    modifyMVar_ clients $ \c -> do
-      let cl = addClient_ conn c
-      WS.sendTextData conn ("Client Connected" :: Text)
-      return cl
-    WS.sendTextData conn (msg :: Text)
-    -- clients <- readMVar state
---    case msg of
---        _ | True -> WS.sendTextData conn (msg :: Text)
---        _ | not (prefix `T.isPrefixOf` msg) ->
---             WS.sendTextData conn ("Wrong announcement" :: Text)
---            | any ($ fst client)
---              [T.null, T.any isPunctuation, T.any isSpace] ->
---                WS.sendTextData conn ("Name cannot " <>
---                  "contain punctuation or whitespace, and " <>
---                  "cannot be empty" :: Text)
---            | clientExists client clients ->
---              WS.sendTextData conn ("User already exists" :: Text)
---            | otherwise -> flip finally disconnect $ do
---                modifyMVar_ state $ \s -> do
---                  let s' = addClient client s
---                  WS.sendTextData conn $ "Welcome! Users: " <> T.intercalate ", " (map fst s)
---                  broadcast (fst client <> " joined") s'
---                  return s'
---                talk client state
---          where
---            prefix     = "Hi! I am "
---              client     = (T.drop (T.length prefix) msg, conn)
---              disconnect = do
---                s <- modifyMVar state $ \s ->
---                  let s' = removeClient client s in return (s', s')
---                broadcast (fst client <> " disconnected") s
--- The talk function continues to read messages from a single client until he
--- disconnects. All messages are broadcasted to the other clients.
+      modifyMVar_ clients $ \c -> do
+        let cl = addClient_ conn c
+        T.putStrLn "Client Connected."
+        return cl
+      WS.sendTextData conn ("Client connected" :: Text)
+      talk conn clients
 
--- talk :: Client -> MVar ServerState -> IO ()
--- talk (user, conn) state = forever $ do
---     msg <- WS.receiveData conn
---     readMVar state >>= broadcast
---         (user `mappend` ": " `mappend` msg)
+talk :: WS.Connection -> MVar Clients -> IO ()
+talk conn clients = forever $ do
+  msg <- WS.receiveData conn
+  T.putStrLn (T.append (T.pack "Client -> Server : ") msg)
+  T.putStrLn (T.append (T.pack "Client <- Server : ") msg)
+  WS.sendTextData conn (msg :: Text)
