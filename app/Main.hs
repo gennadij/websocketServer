dbfe2dad2a3e544ac71b3cfb4e742a4565b3b610
Gennadi <gennadiheimann@gmail.com>
2021-04-13 00:12:13 +0200

    Json encoding und decoding implementiert

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,4 @@
---module Main where
-
---
-
---main :: IO ()
---main = someFunc
-
---websockets example
--- ==================
-
--- This is the Haskell implementation of the example for the WebSockets library. We
--- implement a simple multi-user chat program. A live demo of the example is
--- available [here](/example/client.html).  In order to understand this example,
--- keep the [reference](/reference/) nearby to check out the functions we use.
-
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Data.Char (isPunctuation, isSpace, isDigit)
@@ -26,6 +12,9 @@ import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
 
 import Lib ( calcExactRoot )
+import Data.Aeson
+import GHC.Generics
+
 -- new TestTyp
 type Clients = [WS.Connection]
 
@@ -38,6 +27,15 @@ newClients = []
 -- new addClient
 addClient :: WS.Connection -> Clients -> Clients
 addClient conn clients = conn : clients
+
+-- Test Json
+data CalcExactRoot = CalcExactRoot{
+      test :: !Text 
+} deriving (Show, Generic)
+
+instance FromJSON CalcExactRoot
+instance ToJSON CalcExactRoot
+
 
 main :: IO ()
 main = do
@@ -61,8 +59,11 @@ application clients pending = do
       talk conn clients
 
 talk :: WS.Connection -> MVar Clients -> IO ()
-talk conn clients = forever $ do 
+talk conn clients = forever $ do  
   msg <- WS.receiveData conn
+  let Just res = decode (WS.toLazyByteString msg) :: Maybe CalcExactRoot
+  print res
+  putStrLn $ "Decode: " ++ (show (decode $ WS.toLazyByteString msg :: Maybe CalcExactRoot))
   handleMsg msg conn
   -- T.putStrLn (T.append (T.pack "Client -> Server : ") msg)
   -- WS.sendTextData conn (
@@ -76,8 +77,9 @@ convertTextToInt st = read (T.unpack st) :: Int
 handleMsg :: Text -> WS.Connection -> IO ()
 handleMsg msg conn = case msg of
   _ | isNummeric $ T.unpack msg -> WS.sendTextData conn (T.pack "Response: " `T.append` T.pack (calcExactRoot(convertTextToInt (msg :: Text))))
-    | otherwise -> WS.sendTextData conn ("Undefinierte Eingabe" :: Text)
+    | otherwise -> WS.sendTextData conn (encode (CalcExactRoot{test = "response"}))
 
 isNummeric :: String -> Bool
 isNummeric [] = False
 isNummeric xs = all isDigit xs
+
